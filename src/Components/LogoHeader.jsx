4ee6fd973c9ece6e8a0917d8b53f78bdaf3c7f55
tranthainhan <tranthainhan1997@gmail.com>
2019-11-14 10:39:49 +0700

    create component logo and folder img in folder public

---
+++ src/Components/LogoHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogoHeader = () => {
+    return (
+        <div className='logo-header'>
+            <img src="./img/logo.png " alt="Hình logo"/>
+        </div>
+    )
+}
+
+export default LogoHeader
