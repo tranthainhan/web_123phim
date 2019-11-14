ee86e58fce4900110f35812f3f98e1dbd269cb88
Enzo0801 <vodung0801@gmail.com>
2019-11-14 16:59:11 +0700

    Done Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+//IMPORT COMPONENT
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <div className="App">
-      
+      <Footer/>
     </div>
   );
 }
