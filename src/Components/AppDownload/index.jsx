5500c8e6b49be8d0c491a36566d578fc44622cf7
Enzo0801 <vodung0801@gmail.com>
2019-11-16 17:12:11 +0700

    Responsive AppDownload Component With Many Popular Mobile Phone

--- src/Components/AppDownload/index.jsx
+++ src/Components/AppDownload/index.jsx
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import './style.scss';
 
 import mobile from "../../Assets/img/phone_hand.png";
+import slide0 from "../../Assets/img/slide0.png";
+import slide1 from "../../Assets/img/slide1.png";
+import slide2 from "../../Assets/img/slide2.png";
+import slide3 from "../../Assets/img/slide4.png";
+import slide4 from "../../Assets/img/slide5.png";
 
 export default class AppDownload extends Component {
     render() {
@@ -10,7 +15,7 @@ export default class AppDownload extends Component {
             <div className="app_download">
                 <div className="app_download_container container">
                     <div className="row">
-                        <div className="col-md-6 col-sm-12 col-xs-12 left text-left pr-0">
+                        <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 left text-left px-0">
                             <p className="textLeft">Ứng dụng tiện lợi dành cho</p>
                             <p className="textLeft">người yêu điện ảnh</p>
                             <br />
@@ -22,8 +27,27 @@ export default class AppDownload extends Component {
                             <p className="textAppUnder">123Phim có hai phiên bản <a className="tag" target="_blank" rel="noopener noreferrer" href="https://itunes.apple.com/us/app/123phim-mua-ve-lien-tay-chon/id615186197?mt=8">iOS</a> &amp;&nbsp; <a className="tag" target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=vn.com.vng.phim123">Android</a></p>
                         </div>
 
-                        <div className="col-md-6 col-sm-12 col-xs-12 right pl-0">
-                            <img className="imgResponsive" src={mobile} alt="" />
+                        <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 right px-0">
+                            <img className="imgResponsive w-100" src={mobile} alt="" />
+                            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img src={slide0} className="d-block" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={slide1} className="d-block" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={slide2} className="d-block" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={slide3} className="d-block" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={slide4} className="d-block" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
