f92badb2aef4880660293da9b8c62e2183e95a25
Enzo0801 <vodung0801@gmail.com>
2019-12-03 16:53:59 +0700

    Session

--- src/Containers/Session/index.jsx
+++ src/Containers/Session/index.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { connect } from "react-redux";
@@ -53,22 +51,29 @@ const useStyles = makeStyles(theme => ({
 
 function Session(props) {
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+    const [value1, setValue1] = React.useState(0);
+    const [value2, setValue2] = React.useState(0);
+    const [liValue, setLiValue] = React.useState(0);
     const [filterShowTimesList, setFilterShowTimesList] = useState([]);
+    const [filterAddressList, setFilterAddressList] = useState([]);
 
     useEffect(() => {
         if (_.isEmpty(props.cinemaList)) {
             props.getCinema();
             props.getShowTimes('GP09');
         }
-        // else {
-        //     onClickLogo(props.cinemaList[0].maHeThongRap);
-        // }
+        else {
+            onClickLogo(props.cinemaList[0].maHeThongRap);
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.cinemaList]);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
+    const handleChange1 = (event, newValue) => {
+        setValue1(newValue);
+    };
+
+    const handleChange2 = (event, newValue) => {
+        setValue2(newValue);
     };
 
     const onClickLogo = (maRap) => {
@@ -81,10 +86,33 @@ function Session(props) {
         ]);
     }
 
-    const toggleActive = (index) => {
-        setValue({
-            value: index,
+    const onClickAddress = (maCumRap) => {
+        const filterList = filterShowTimesList.map((wrap) => {
+            return wrap.map((item) => {
+                return item.lstCumRap.filter((film) => {
+                    return film.maCumRap === maCumRap
+                })
+            })
         })
+
+        setFilterAddressList(filterList);
+    }
+
+    const toggleActiveCol1 = (index) => {
+        setValue1(index);
+    }
+
+    const toggleActiveCol2 = (index) => {
+        setValue2(index);
+    }
+
+    const toggleActiveLi = (index) => {
+        setLiValue(index);
+    }
+
+    const convertTime = (time) => {
+        var d = new Date(time + 'Z');
+        return d.getUTCHours() + ":" + d.getUTCMinutes();
     }
 
     return (
@@ -92,14 +120,14 @@ function Session(props) {
             <Tabs
                 orientation="vertical"
                 variant="scrollable"
-                value={value}
-                onChange={handleChange}
+                value={value1}
+                onChange={handleChange1}
                 aria-label="Vertical tabs example"
                 className={classes.tabs}
             >
                 {
                     props.cinemaList.map((item, index) => {
-                        return <img className="session_logoCinema" src={item.logo} alt="" onClick={() => { onClickLogo(item.maHeThongRap) }} />
+                        return <img className={index === value1 ? "session_logoCinema active" : "session_logoCinema"} src={item.logo} alt="" onClick={() => { onClickLogo(item.maHeThongRap); toggleActiveCol1(index) }} />
                     })
                 }
             </Tabs>
@@ -108,14 +136,49 @@ function Session(props) {
                 // console.log(filterShowTimesList)
                 filterShowTimesList.map((wrap) => {
                     return wrap.map((item) => {
-                        return <TabPanel value={value} index={value}>
+                        return <TabPanel className="wrap23" value={value1} index={value1}>
+                            <Tabs
+                                orientation="vertical"
+                                variant="scrollable"
+                                value={value2}
+                                onChange={handleChange2}
+                                aria-label="Vertical tabs example"
+                                className="colum2"
+                            >
+                                {
+                                    item.lstCumRap.map((film, index) => {
+                                        return <div className={index === value2 ? "session_addressCinema_wrap active" : "session_addressCinema_wrap"} onClick={() => { onClickAddress(film.maCumRap); toggleActiveCol2(index) }}>
+                                            <p className="titleCinema">{film.tenCumRap}</p>
+                                            <p className="addressCinema">{film.diaChi}</p>
+                                            <p className="detailCinema"><a href="/">[chi tiết]</a></p>
+                                        </div>
+                                    })
+                                }
+                            </Tabs>
+
                             {
-                                item.lstCumRap.map((film) => {
-                                    return <div className="session_addressCinema_wrap">
-                                        <p className="titleCinema">{film.tenCumRap}</p>
-                                        <p className="addressCinema">{film.diaChi}</p>
-                                        <p className="detailCinema">[chi tiết]</p>
-                                    </div>
+                                filterAddressList.map((wrap1) => {
+                                    return wrap1.map((wrap2) => {
+                                        return wrap2.map((item) => {
+                                            return item.danhSachPhim.map((film) => {
+                                                return <TabPanel className="colum3" value={value2} index={value2}>
+                                                    <div className="colum3_wrap">
+                                                        <div className="title" ><p>{film.tenPhim}</p></div>
+                                                        <ul className="showTimes_wrap">
+                                                            {
+                                                                film.lstLichChieuTheoPhim.map((item, index) => {
+                                                                    const prefix = '2019-01-01T';
+                                                                    if (item.ngayChieuGioChieu.slice(0, 11) === prefix) {
+                                                                        return <li key={index} onClick={() => { toggleActiveLi(index) }} className={index === liValue ? "showTimes active" : "showTimes"}>{convertTime(item.ngayChieuGioChieu)}</li>
+                                                                    }
+                                                                })
+                                                            }
+                                                        </ul>
+                                                    </div>
+                                                </TabPanel>
+                                            })
+                                        })
+                                    })
                                 })
                             }
                         </TabPanel>
