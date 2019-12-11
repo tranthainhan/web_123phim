978cd4a18606d4a9b588546003f3f0972ca434bd
Enzo0801 <vodung0801@gmail.com>
2019-12-11 15:04:21 +0700

    Session

--- src/Containers/Session/index.jsx
+++ src/Containers/Session/index.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import classNames from 'classnames'
 import { connect } from "react-redux";
 import _ from "lodash";
 import { getCinema } from "../../Actions/Cinema";
@@ -9,81 +10,115 @@ import { getShowTimes } from "../../Actions/Cinema";
 import "./style.scss";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      <Box p={2}>{children}</Box>
-    </Typography>
-  );
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            <Box p={2}>{children}</Box>
+        </Typography>
+    );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired
 };
 
 function Session(props) {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
+    let refs = useRef([]);
+    let refs2 = useRef([]);
 
-  useEffect(() => {
-    if (_.isEmpty(props.cinemaList) && _.isEmpty(props.showTimesList)) {
-      props.getCinema();
-      props.getShowTimes("GP09");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        if (_.isEmpty(props.cinemaList) && _.isEmpty(props.showTimesList)) {
+            props.getCinema();
+            props.getShowTimes("GP09");
+        }
 
-  const toggleActiveCol = index => {
-    setValue(index);
-  };
+    }, []);
 
-  // const convertTime = time => {
-  //   var d = new Date(time + "Z");
-  //   return d.getUTCHours() + ":" + d.getUTCMinutes();
-  // };
+    const handleChange = newValue => {
+        setValue(newValue);
+    };
 
-  return (
-    <div className="wrap">
-      <div className="col1_wrap">
-        {props.cinemaList.map((item, index) => {
-          return (
-            <div className="session_logoCinema_wrap">
-              <img
-                className={
-                  index === value
-                    ? "session_logoCinema active"
-                    : "session_logoCinema"
+    const toggleActive = location => {
+        refs.current.forEach((item, index) => {
+            if (location === index) {
+                item.classList.add("active");
+            } else item.classList.remove("active");
+        });
+    };
+    const toggleActive2 = location => {
+        console.log()
+        refs2.current.map((item,index) => location === index ? item.classList.add('active') : item.classList.remove('active'))
+    };
+
+    const convertTime = time => {
+        var d = new Date(time + "Z");
+        return d.getUTCHours() + ":" + d.getUTCMinutes();
+    };
+    // console.log( props.showTimesList)
+    return (
+        <div className="wrap">
+            <div className="col1_wrap">
+                {props.cinemaList && props.cinemaList.map((item, index) => {
+                    return (
+                        <div key={index} className="session_logoCinema_wrap">
+                            <img
+                                className={classNames("session_logoCinema", { 'active': index === 0 })}
+                                src={item.logo}
+                                alt=""
+                                onClick={() => {
+                                    handleChange(index); toggleActive(index);
+                                }}
+                                ref={img => (refs.current[index] = img)}
+                            />
+                        </div>
+                    );
+                })
                 }
-                src={item.logo}
-                alt=""
-                onClick={() => {
-                  toggleActiveCol(index);
-                }}
-              />
             </div>
-          );
-        })}
-      </div>
 
-      <div className="col2_wrap">{}</div>
-    </div>
-  );
+            <div className="col2_wrap">
+                {
+                    props.showTimesList.map((wrap, index) => {
+                        return <TabPanel value={value} index={index} key={index} >
+                            {
+                                wrap.lstCumRap.map((item, index) => {
+                                    return <div key={index} ref={div => refs2.current[index] = div} className={classNames("session_addressCinema_wrap", { 'active': index === 0 })} onClick={() => { toggleActive2(index);  }}>
+                                        <p className="titleCinema">{item.tenCumRap}</p>
+                                        <p className="addressCinema">{item.diaChi}</p>
+                                        <p className="detailCinema"><a href="/">[chi tiết]</a></p>
+                                    </div>
+                                })
+                            }
+                        </TabPanel>
+                    })
+                }
+            </div>
+
+            <div className="col3_wrap">
+                {
+
+                }
+            </div>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
-  return {
-    cinemaList: state.cinemaList,
-    showTimesList: state.showTimes
-  };
+    return {
+        cinemaList: state.cinemaList,
+        showTimesList: state.showTimes
+    };
 };
 
 export default connect(mapStateToProps, { getCinema, getShowTimes })(Session);
